1ca97c1ce219b729501ad455ae2b625086d86134
virtualmix <basiledeplante@gmail.com>
2021-09-10 14:51:40 -0700

    first commit

---
+++ polls/models.py
@@ -0,0 +1,43 @@
+import datetime
+
+<<<<<<< HEAD
+from django.contrib import admin
+from django.db import models
+from django.utils import timezone
+=======
+from django.db import models
+from django.utils import timezone
+
+from django.contrib import admin
+>>>>>>> e8af1b33e46347bcc5784213da8b17b1c0c69b2c
+
+
+class Question(models.Model):
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
